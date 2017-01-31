8a92e2922db346d963c57dc6409e7c40d80f58ae
Herry Wang <tech.herry@gmail.com>
2017-01-30 19:09:13 -0800

    using get method to retrive ENV vars

--- send_message.py
+++ send_message.py
@@ -15,31 +15,37 @@ spark_urls = {
 spark_headers = {}
 spark_headers["Content-type"] = "application/json"
 
+
 def get_roomId():
     '''
     Determine the roomId to send the message to.
     '''
+    roomid = os.environ.get("PLUGIN_ROOMID")
+    roomname = os.environ.get("PLUGIN_ROOMNAME")
+
 
     # If an explict roomId was provided as a varg, verify it's a valid roomId
-    if os.environ.get("PLUGIN_ROOMID"):
-        if verify_roomId(os.environ["PLUGIN_ROOMID"]):
-            return os.environ["PLUGIN_ROOMID"]
+    if roomid:
+        if verify_roomId(roomid):
+            return roomid
     # If a roomName is provided, send to room with that title
-    elif os.environ.get("PLUGIN_ROOMNAME"):
+    elif roomname:
         # Try to find room based on room name
         response = requests.get(
             spark_urls["rooms"],
-            headers = spark_headers
+            headers=spark_headers
         )
         rooms = response.json()["items"]
-        #print("Number Rooms: " + str(len(rooms)))
+        # print("Number Rooms: " + str(len(rooms)))
         for room in rooms:
-            #print("Room: " + room["title"])
-            if os.environ["PLUGIN_ROOMNAME"] == room["title"]:
+            # print("Room: " + room["title"])
+            if roomname == room["title"]:
                 return room["id"]
 
     # If no valid roomId could be found, raise error
-    raise(LookupError("roomId can't be determined"))
+    raise(
+        LookupError("roomId can't be determined"))
+
 
 def verify_roomId(roomId):
     '''
@@ -49,7 +55,7 @@ def verify_roomId(roomId):
 
     response = requests.get(
         url,
-        headers = spark_headers
+        headers=spark_headers
     )
 
     if response.status_code == 200:
@@ -57,26 +63,47 @@ def verify_roomId(roomId):
     else:
         return False
 
+
 def standard_message():
     '''
     This will create a standard notification message.
     '''
-    status = os.environ["PLUGIN_BUILD_STATUS"]
+    status = os.environ.get("PLUGIN_BUILD_STATUS")
+    repo_fullname = os.environ.get("PLUGIN_REPO_FULL_NAME")
+    author = os.environ.get("PLUGIN_BUILD_AUTHOR")
+    author_email = os.environ.get("PLUGIN_BUILD_AUTHOR_EMAIL")
+    url = os.environ.get("PLUGIN_SYSTEM_LINK_URL")
+    fullname = os.environ.get("PLUGIN_REPO_FULL_NAME")
+    build_number = os.environ.get("PLUGIN_BUILD_NUMBER")
+    build_link = os.environ.get("PLUGIN_BUILD_LINK_URL")
+    build_branch = os.environ.get("PLUGIN_BUILD_BRANCH")
+    build_event = os.environ.get("PLUGIN_BUILD_EVENT")
+    build_msg = os.environ.get("PLUGIN_BUILD_MESSAGE")
     if status == "success":
-        message = "##Build for %s is Successful \n" % (os.environ["PLUGIN_REPO_FULL_NAME"])
-        message = message + "**Build author:** [%s](%s) \n" % (os.environ["PLUGIN_BUILD_AUTHOR"], os.environ["PLUGIN_BUILD_AUTHOR_EMAIL"])
+        message = "##Build for %s is Successful \n" % (
+            repo_fullname)
+        message = message + "**Build author:** [%s](%s) \n" % (
+            author, author_email)
     else:
-        message = "#Build for %s FAILED!!! \n" % (os.environ["PLUGIN_REPO_FULL_NAME"])
-        message = message + "**Drone blames build author:** [%s](%s) \n" % (os.environ["PLUGIN_BUILD_AUTHOR"], os.environ["PLUGIN_BUILD_AUTHOR_EMAIL"])
+        message = "#Build for %s FAILED!!! \n" % (
+            repo_fullname)
+        message = message + "**Drone blames build author:** [%s](%s) \n" % (
+            author, author_email)
 
     message = message + "###Build Details \n"
-    message = message + "* [Build Log](%s/%s/%s)\n" % (os.environ["PLUGIN_SYSTEM_LINK_URL"], os.environ["PLUGIN_REPO_FULL_NAME"], os.environ["PLUGIN_BUILD_NUMBER"])
-    message = message + "* [Commit Log](%s)\n" % (os.environ["PLUGIN_BUILD_LINK_URL"])
-    message = message + "* **Branch:** %s\n" % (os.environ["PLUGIN_BUILD_BRANCH"])
-    message = message + "* **Event:** %s\n" % (os.environ["PLUGIN_BUILD_EVENT"])
-    message = message + "* **Commit Message:** %s\n" % (os.environ["PLUGIN_BUILD_MESSAGE"])
+    message = message + \
+        "* [Build Log](%s/%s/%s)\n" % (url, fullname, build_number)
+    message = message + \
+        "* [Commit Log](%s)\n" % (build_link)
+    message = message + \
+        "* **Branch:** %s\n" % (build_branch)
+    message = message + \
+        "* **Event:** %s\n" % (build_event)
+    message = message + \
+        "* **Commit Message:** %s\n" % (build_msg)
 
     return message
+
 
 def send_message(message_data, message_text):
 
@@ -84,35 +111,37 @@ def send_message(message_data, message_text):
 
     response = requests.post(
         spark_urls["messages"],
-        headers = spark_headers,
-        json = message_data
+        headers=spark_headers,
+        json=message_data
     )
 
     return response
 
+
 def main():
+    token = os.environ.get("PLUGIN_AUTH_TOKEN")
+    if token is None:
+        raise(LookupError(
+            "Requires valid PLUGIN_AUTH_TOKEN to be provided."))
 
     # Prepare headers and message objects
-    spark_headers["Authorization"] = "Bearer %s" % (os.environ["PLUGIN_AUTH_TOKEN"])
+    spark_headers["Authorization"] = "Bearer %s" % (token)
     spark_message = {}
 
     # Determine destination for message
     try:
         # First look for a valid roomId or roomName
-        roomId = get_roomId()
-        spark_message["roomId"] = roomId
+        spark_message["roomId"] = get_roomId()
     except LookupError:
-        # See if a personEmail was provided
-        if os.environ["PLUGIN_AUTH_TOKEN"]:
-            spark_message["toPersonEmail"] = os.environ["PLUGIN_AUTH_TOKEN"]
-        else:
-            raise(LookupError("Requires valid roomId, roomName, or personEmail to be provided.  "))
-
-    # Send Standard message
+        raise(LookupError(
+            "Requires valid roomId, roomName, or personEmail to be provided.  "))
+        # Send Standard message
     standard_notify = send_message(spark_message, standard_message())
     if standard_notify.status_code != 200:
-        print(standard_notify.json()["message"])
-        raise(SystemExit("Something went wrong..."))
+        print(
+            standard_notify.json()["message"])
+        raise(
+            SystemExit("Something went wrong..."))
 
 if __name__ == "__main__":
     main()
